ca6a2e33f4e1ce03adb74c0c3abaa25cae156c37
Josep Maria Camps i Riba <jmcamps@gmail.com>
2014-06-16 12:37:00 +0200

    Full implementation with all tests passing

--- src/main/scala/cat/xampi/kata/roman/RomanNumeralsConverter.scala
+++ src/main/scala/cat/xampi/kata/roman/RomanNumeralsConverter.scala
@@ -2,5 +2,34 @@ package cat.xampi.kata.roman
 
 object RomanNumeralsConverter {
 
-  def toRoman(arabic:Int): String = { "I" }
+  def toRoman(arabic: Int): String = {
+    val romanDigits = Map(
+      1 -> "I", 5 -> "V",
+      10 -> "X", 50 -> "L",
+      100 -> "C", 500 -> "D",
+      1000 -> "M",
+      4 -> "IV", 9 -> "IX",
+      40 -> "XL", 90 -> "XC",
+      400 -> "CD", 900 -> "CM")
+    
+    def conversion(position: Int) (initialSymbol: String, middleSymbol: String, endSymbol: String): String = {
+      val digit = arabic % (10*position) / position match {
+        case 0 => "" 
+        case 1 => initialSymbol
+        case 2 => initialSymbol + initialSymbol
+        case 3 => initialSymbol + initialSymbol + initialSymbol
+        case 4 => initialSymbol + middleSymbol
+        case 5 => middleSymbol 
+        case 6 => middleSymbol + initialSymbol
+        case 7 => middleSymbol + initialSymbol + initialSymbol
+        case 8 => middleSymbol + initialSymbol + initialSymbol + initialSymbol
+        case 9 => initialSymbol + endSymbol 
+        
+      }
+      digit
+    } 
+    
+    conversion(1000)("M", "?", "?") + conversion(100)("C", "D", "M") + conversion(10)("X", "L", "C") + conversion(1)("I", "V", "X")
+    
+  }
 }
--- src/test/scala/cat/xampi/kata/roman/RomanNumeralsConverter.scala
+++ src/test/scala/cat/xampi/kata/roman/RomanNumeralsConverter.scala
@@ -10,4 +10,63 @@ class RomanNumeralsConverterSpec extends FlatSpec with ShouldMatchers {
   "1" should "be I" in {
     RomanNumeralsConverter toRoman 1 should be === "I"
   }
+  
+  "2" should "be II" in {
+    RomanNumeralsConverter toRoman 2 should be === "II"
+  }
+  
+  "3" should "be III" in {
+    RomanNumeralsConverter toRoman 3 should be === "III"
+  }
+  
+  "4" should "be IV" in {
+    RomanNumeralsConverter toRoman 4 should be === "IV" 
+  }
+  
+  "5" should "be V" in {
+    RomanNumeralsConverter toRoman 5 should be === "V" 
+  }
+  "6" should "be VI" in {
+    RomanNumeralsConverter toRoman 6 should be === "VI" 
+  }
+  "7" should "be VII" in {
+    RomanNumeralsConverter toRoman 7 should be === "VII" 
+  }
+  "8" should "be VIII" in {
+    RomanNumeralsConverter toRoman 8 should be === "VIII" 
+  }
+  "9" should "be IX" in {
+    RomanNumeralsConverter toRoman 9 should be === "IX" 
+  }
+  "10" should "be X" in {
+    RomanNumeralsConverter toRoman 10 should be === "X" 
+  }
+  
+ "11" should "be XI" in {
+    RomanNumeralsConverter toRoman 11 should be === "XI" 
+  }
+ 
+ "12" should "be XII" in {
+    RomanNumeralsConverter toRoman 12 should be === "XII" 
+  }
+ 
+ "16" should "be XVI" in {
+    RomanNumeralsConverter toRoman 16 should be === "XVI" 
+  }
+ 
+ "19" should "be XIX" in {
+    RomanNumeralsConverter toRoman 19 should be === "XIX" 
+  }
+ 
+ "40" should "be XL" in {
+    RomanNumeralsConverter toRoman 40 should be === "XL" 
+  }
+ 
+ "946" should "be CMXLVI" in {
+    RomanNumeralsConverter toRoman 946 should be === "CMXLVI" 
+  }
+ 
+ "3497" should "be MMMCDXCVII" in {
+    RomanNumeralsConverter toRoman 3497 should be === "MMMCDXCVII" 
+  }
 }
